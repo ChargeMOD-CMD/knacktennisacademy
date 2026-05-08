d37695b4e074e0e671f22580e1c4aafcb8bb14fe
ChargeMOD-CMD <techfreetotalk@gmail.com>
2026-05-08 15:25:18 +0530

    fix: refine SPA build and add tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/knacktennisacademy/",
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   build: {
